ad7d94de06613ccba3f3b7bd7cc134d4c574c0d9
Austin LePorin <austinleporin@gmail.com>
2022-07-21 11:57:10 -0400

    Framework for front end

---
+++ client/src/components/Food.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Food = () => {
+  return (
+    <div className='food'>
+      <div className='foodImg'>
+        <img src="" alt="" />
+       <div className='foodInfo'>
+        <h3>//food.name</h3>
+        <p>//food.description</p>
+        </div> 
+      </div>
+    </div>
+  )
+}
+
+export default Food
---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Rides from '../components/Rides'
+import Food from '../components/Food'
+import GiftShop from '../components/GiftShop'
+import Tickets from '../components/Tickets'
+
+const Home = () => {
+  return <div>Home</div>
+}
+
+export default Home
