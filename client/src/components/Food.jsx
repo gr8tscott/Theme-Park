02a6025f232e8167c19b6c57c162a1de950604d7
Austin LePorin <austinleporin@gmail.com>
2022-07-22 11:42:12 -0400

    IDK

--- client/src/components/Food.jsx
+++ client/src/components/Food.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 
-const Food = () => {
+const Food = (props) => {
   return (
-    <div className='food'>
-      <div className='foodImg'>
-        <img src="" alt="" />
-       <div className='foodInfo'>
-        <h3>//food.name</h3>
-        <p>//food.description</p>
-        </div> 
+    <div className='foodCard'>
+        {
+          props.food.map((food) => (
+            <div className='food-card' key={food.id}>
+                  <h3>{food.name}</h3>
+                  <p>{food.location}</p>
+                
+              {/* {props.meals.map((meal)=> (
+               <h2>{meal}</h2>
+              ))} */}
+            </div>
+          ))
+        }
       </div>
-    </div>
-  )
-}
+    )
+  }
 
 export default Food
